ed00d06a5da847ae432b9bcdab4725a6e244173e
Deotama Taqy <deotama11@gmail.com>
2024-02-14 09:44:40 +0700

    second commit

--- BelajarAPI/Context/DataContexts.cs
+++ BelajarAPI/Context/DataContexts.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace BelajarAPI.Context;
 
-public class DataContexts
+public class DataContexts : DbContext
 {
+	public DataContexts(DbContextOptions<DataContexts> options) : base(options)
+	{
+	}
+	
+	
 	
 }
--- BelajarAPI/Program.cs
+++ BelajarAPI/Program.cs
@@ -12,8 +12,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	Console.WriteLine("Running in Development...");
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
